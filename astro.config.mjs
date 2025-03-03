ae339d055764a032203fcf75c7526a9d3f13e55b
Marko Vidaković <markovidakovic29@gmail.com>
2025-03-03 21:32:10 +0100

    -

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,9 @@
-// @ts-check
 import { defineConfig } from "astro/config";
-import tailwindcss from "@tailwindcss/vite";
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
+import tailwind from "@astrojs/tailwind";
 
-// https://astro.build/config
 export default defineConfig({
-  site: "https://markovidakovic.com",
-  vite: {
-    plugins: [tailwindcss()],
-  },
+  site: "https://astro-nano-demo.vercel.app",
+  integrations: [mdx(), sitemap(), tailwind()],
 });
